76cc1487d03b5589dd0affe1c8d67fe472827618
NormanCao2702 <norman.cao2702@gmail.com>
2024-06-02 23:03:43 -0700

    finalize part 1 and 1 func part 2

--- Assignment1/README.md
+++ Assignment1/README.md
@@ -1,4 +1,13 @@
 # Assignment 1
-There are 2 parts: Integer Iterator and Fifteen Puzzle.
+There are 2 parts: **Integer Iterator** and **Fifteen Puzzle**.
 
-1. Integer Iterator Interface:
+## 1. Integer Iterator:
+**To do task:** Implement 3 classes from the Interface IntegerIterator under integeriterators package which are:
+[x] ArrayIterator
+[x] PrimeNumbersIterator
+[x] RangeIterator
+
+**Side note:** The interface IntegerIterator is an extension of Iterator<Integer>
+
+## 2. Fifteen Puzzle
+
--- Assignment1/src/fifteenpuzzle/FifteenPuzzle.java
+++ Assignment1/src/fifteenpuzzle/FifteenPuzzle.java
@@ -12,7 +12,23 @@ public class FifteenPuzzle {
 	// size of board
 	public final static int SIZE = 4;
 
-	
+	private int[][] board;
+
+	/**
+	 Bad board:
+	 Duplicate number
+	 Number larger than 15
+	 **/
+	private void checkBoard() throws BadBoardException{
+		Set<Integer> seenValues = new HashSet<>();
+		for(int row = 0; row < SIZE; row++){
+			for(int col = 0; col < SIZE; col++){
+				int value = this.board[row][col];
+				if(value < 0 || value > 15) throw new BadBoardException("Value out of range: " + value);
+				if (!seenValues.add(value)) throw new BadBoardException("Duplicate value found: " + value);
+			}
+		}
+	}
 	/**
 	 * @param fileName 
 	 * @throws FileNotFoundException if file not found
@@ -21,9 +37,32 @@ public class FifteenPuzzle {
 	 */
 	public FifteenPuzzle(String fileName) throws IOException, BadBoardException {
 		// TODO implement me
+		board = new int[SIZE][SIZE];
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			for(int row = 0; row < SIZE; row++){
+				String line = br.readLine();
+				if(line.length() != 11) throw new BadBoardException("Incorrect Line length in row"+ row);
+				for (int col = 0; col < SIZE; col++){
+					String part = line.substring(col * 3, col*3+2).trim();
+					if(part.isEmpty()) board[row][col] = 0;
+					else{
+						try{
+							board[row][col] = Integer.parseInt(part);
+						} catch (NumberFormatException e){
+							throw new BadBoardException("Invalid number format in tile [" + row + "]" + "[" + col + "]");
+						}
+					}
+				}
+			}
+		}catch (FileNotFoundException e) {
+			throw e;
+		}
+		catch (IOException e){
+			throw new IOException("Error reading file: " + fileName, e);
+		}
+		checkBoard();
 	}
 
-	
 	/**
 	 * Get the number of the tile, and moves it to the specified direction
 	 * 
@@ -47,6 +86,20 @@ public class FifteenPuzzle {
 	public String toString() {
 		// TODO Auto-generated method stub
 		// TODO implement me
-		return super.toString();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				if (board[i][j] == 0) {
+					sb.append("  "); // Two spaces for empty tile
+				} else {
+					sb.append(String.format("%2d", board[i][j]));// Format number with two spaces, if 1 digit add a space, 2 digit display as they are
+				}
+				if (j < SIZE - 1) {
+					sb.append(" "); // Add space between numbers
+				}
+			}
+			sb.append("\n"); // Add newline after each row
+		}
+		return sb.toString();
 	}
 }
